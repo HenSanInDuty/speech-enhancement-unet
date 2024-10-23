e1b102da6b85a5ab51ca96d3f1dc734129a32d5b
Hen <heninduty@gmail.com>
2024-10-23 16:24:57 +0700

    feat: first init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# speech-enhancement-unet
+## DEMO SPP
+## Run application
+1. Python version = 3.12.5
+2. Run application with command:
+   - streamlit run gui.py
+
+## Youtube demo
+https://drive.google.com/drive/u/0/folders/1fwYz7HtSFfbAssKZfsY0W1t7dKQKV08n
+
+## Train model
+Go to this link: https://drive.google.com/drive/u/0/folders/1fwYz7HtSFfbAssKZfsY0W1t7dKQKV08n
+and Run file **Train_denoise.ipynb** in folder **notebook**
---
+++ gui.py
@@ -0,0 +1,67 @@
+import asyncio
+import io
+import copy
+import streamlit as st
+import os
+import librosa
+import tempfile
+import soundfile as sf
+import numpy as np
+from util import prediction_denoise
+
+async def main():
+    st.title(':musical_note: Speech Enhancement')
+    st.subheader('Remove your audio background noise using Artificial Intelligence')
+    
+    # Input file
+    input = st.file_uploader("Upload your audio")
+    
+    if st.button("Denoise audio"):
+        original_input = copy.copy(input)
+        tmp = io.BytesIO(original_input.read())
+        original_audio, original_sr = librosa.load(tmp)
+        
+        # Get duration of audio
+        S = librosa.stft(original_audio)
+        min_duration = librosa.get_duration(S=S, sr=original_sr)
+        
+        # Inference
+        working_path = os.getcwd()
+        weights_path = os.path.join(working_path, "model", "unet")
+        name_model = "mod_unet_last_weights"
+        frame_length = 8064
+        hop_length_frame = 8064
+        n_fft = 255
+        hop_length_fft = 63
+        
+        denoise, sr = prediction_denoise.prediction(weights_path, name_model, input, original_sr, min_duration, frame_length, hop_length_frame, n_fft, hop_length_fft)
+
+        col1, col2 = st.columns(2)
+        
+        with col1: 
+            st.write("Original Audio")
+            trim_audio, _ = librosa.effects.trim(original_audio)
+            st.audio(trim_audio, sample_rate=original_sr)
+            
+        with col2:
+            st.write("Denoise Audio")
+            st.audio(denoise[0, :], sample_rate=sr)
+            
+        # Download session
+        temp_audio = io.BytesIO()
+        sf.write(temp_audio, denoise[0,:], sr, format='WAV')
+        temp_audio.seek(0)
+        
+        st.download_button("Download Denoise Audio", temp_audio, f"audio_denoise.wav", mime="audio/wav")
+
+if __name__ == '__main__':
+
+    st.set_page_config(
+        page_title="Noise Reduction",
+        page_icon="🤖",
+        layout="wide",
+        initial_sidebar_state="collapsed",
+    )
+    
+    if __name__ == '__main__':
+        asyncio.run(main())
